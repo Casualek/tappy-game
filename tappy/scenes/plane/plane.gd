b232d4f2ea706733392f048f6ac9feb8a0dced15
Casualek <129328915+Casualek@users.noreply.github.com>
2024-11-23 21:55:04 +0100

    Add files via upload

---
+++ tappy/scenes/plane/plane.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+const GRAVITY: float = 1500.0
+const POWER: float = -400.0
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+@onready var animated_sprite: AnimatedSprite2D = $animatedSprite
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	velocity.y += GRAVITY * delta 
+	
+	_fly()
+	
+	move_and_slide()
+	if is_on_ceiling() == true:
+		die()
+
+	if is_on_floor() == true:
+		die()
+
+func die() -> void:
+	animated_sprite.stop()
+	set_physics_process(false)
+
+func _fly() -> void:
+	if Input.is_action_just_pressed("fy") == true:
+		velocity.y = POWER
+		animation_player.play("power")
